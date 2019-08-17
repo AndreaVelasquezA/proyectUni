bde21f20d54aa3ad4c039525c5ea5a4b7da85a5b
Andrea <andrea.velasquez.2019@upb.edu.co>
2019-08-17 04:59:39 +0200

    primer commit

---
+++ numeromayor.py
@@ -0,0 +1,20 @@
+a=int(input("digite el numero:"))
+b=int(input("digite el numero:"))
+c=int(input("digite el numero:"))
+
+if a>b:
+     if a>c:
+       print("el numero mayor es:",a)  
+     else:
+       print("el numero mayor es",c)  
+else:
+ if b>c:
+   print("el numero mayor es:",b)
+ else:
+   print("el numero mayor es:",c)
+
+
+
+
+
+    
